2d0dc5acaf237d9537716e65868278a41edd0630
Max <167529974+Chester-xx@users.noreply.github.com>
2025-07-14 00:49:06 +0200

    new: print functions within log header
    new print and printf functions for logging and ease of printing with multiple arguments

--- apps/app1/main.cpp
+++ apps/app1/main.cpp
@@ -1,8 +1,14 @@
 // launch.json config to app1.exe | when working on other projects remember to change the exe name
-#include <iostream>
+#include "log.h"
 #include "math.h"
 
 int main() {
+
+    printf("hello");
+    std::string str = "asbd uasdu";
+    printf("how are you doing?", "\n", 87, 78.04f, str);
+    printf();
+    print();
 
     return EXIT_SUCCESS;
 }
